0fa7d928f3af026017dd778040645a4ce0ad4c21
Preetha Chandramohan <preetha.chandramohan@perficient.com>
2023-03-31 16:46:07 +0530

    APP ID

--- src/scribos-valigate/Scribos.jsx
+++ src/scribos-valigate/Scribos.jsx
@@ -38,7 +38,7 @@ function Scribos() {
     initialize(
       "valigate",
       // "<ENTER_THE_APP_ID_PROVIDED_BY_SCRIBOS_HERE>",
-      "MDJN_STAGE_R1TL9G5Q2LXK924DK7QV",
+      "MDJN_PROD_476JR5HMS6GFJV73649K",
       `${process.env.PUBLIC_URL}/valigate/worker.f029c5c.js`,
       en
     );
@@ -61,7 +61,6 @@ function Scribos() {
         console.log("ExitCodes", exitCode);
         break;
       case EExitCodes.RESULT_REPORT:
-        alert(JSON.stringify(result, null, 2));
         if (result.REPORT_STATUS && result.REPORT_STATUS === 'success') {
           setShowThanks(true);
           setShowNSForm(false);
