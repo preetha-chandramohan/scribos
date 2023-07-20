6699707e7cd018192e9b2735dfaa952454416af5
Preetha Chandramohan <preetha.chandramohan@perficient.com>
2023-07-20 13:48:21 +0530

    Market state toggle

--- src/App.jsx
+++ src/App.jsx
@@ -185,7 +185,8 @@ function App({ userLocaleX }) {
                 />
               </LangageContainer>
               {console.log(short)}
-              {((show && region === MALAYSIA) || (show && region === "Philippines")) && <Scribos short={short} region={region}/>}
+              {(show && region === MALAYSIA) && <Scribos short={short} region={region}/>}
+              {(show && region === "Philippines") && <Scribos short={short} region={region}/>}
               {console.log(region)}
               {console.log(show)}
               {!show && <div>
